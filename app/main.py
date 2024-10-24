6a05b3760f1bb5cf6ca4a75d2f3f271f0279e208
deloppa <97598343+deloppa@users.noreply.github.com>
2024-10-24 03:17:09 +0300

    Configure CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import engine
 from .models import Base
@@ -7,6 +8,21 @@ from .routes import film_router, user_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(film_router, prefix="/films", tags=["films"])
 app.include_router(user_router, tags=["users"])
